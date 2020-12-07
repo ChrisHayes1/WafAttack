d2dd6de23333fff23919fa6477d261e23dc3f36b
John Madrek <haxbox@jmkali.jmkali.net>
2020-12-07 17:34:15 -0600

    DVWA SQL Inject attack working

---
+++ Attack/attack_setup.sh
@@ -0,0 +1,10 @@
+#run with $ sudo sh env_setup.sh
+
+#install python and pip, install requests library
+sudo apt-get update
+sudo apt install python3
+sudo apt install python3-pip
+pip3 install requests
+
+
+
